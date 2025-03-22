05efb4ba43807b7a7330f1bfe8f7b9deaaf51e42
kakasoo <kscodebase@gmail.com>
2025-03-22 13:32:21 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { Agentica } from "@agentica/core";
+import typia from "typia";
+import dotenv from "dotenv";
+import { OpenAI } from "openai";
+
+import { SlackService } from "@wrtnlabs/connector-slack";
+
+dotenv.config();
+
+export const agent = new Agentica({
+  model: "chatgpt",
+  vendor: {
+    api: new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY!,
+    }),
+    model: "gpt-4o-mini",
+  },
+  controllers: [
+    {
+      name: "Slack Connector",
+      protocol: "class",
+      application: typia.llm.application<SlackService, "chatgpt">(),
+      execute: new SlackService(),
+    },
+  ],
+});
+
+const main = async () => {
+  console.log(await agent.conversate("What can you do?"));
+};
+
+main();
